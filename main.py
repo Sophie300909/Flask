9c4d4395e587c33bf0946a1125c2bb3215a0500e
sofatimofeeva <timofeevasofa26@gmail.com>
2025-03-04 21:02:49 +0300

    Результат отбора

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,30 +7,27 @@ app = Flask(__name__)
 def title():
     return "Missiya"
 
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def choice(nickname, level, rating):
     return f'''<!doctype html>
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                       </head>
-                        <title>Варианты выбора</title>
+                        <title>Результаты</title>
                       </head>
                       <body>
-                        <h1>Мое предложение: {planet_name}!</h1>
-                      <h2> Эта планета близка к Земле;</h2>
+                        <h1>Результаты отбора</h1>
+                      <h2>Претендента на участие в миссии {nickname}:</h2>
                         <div class="alert alert-success" role="alert">
-                            <p>На ней много необходимых ресурсов;</p>
+                            <h4 class="alert-heading">Поздравляем! Ваш рейтинг после {level} этапа отбора</h4>
                         </div>
-                        <div class="alert alert-secondary" role="alert">
-                            <p>На ней есть вода и атмосфера;</p>
+                        <div>
+                            <strong>составляет {rating}!</strong>
                         </div>
-                        <div class="alert alert-warning" role="alert">
-                            <p>На ней есть небольшое магнитное поле;</p>
-                        </div>
-                        <div class="alert alert-danger" role="alert">
-                            <p>Наконец, она просто красива!</p>
+                        <div class="alert alert-warning f" role="alert">
+                            <h4 class="alert-heading">Желаем удачи!</h4>
                         </div>
                       </body>
                     </html>'''
