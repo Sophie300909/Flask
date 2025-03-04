a80ed59ce5c5bf8a83fef5d32c729b7f769a45d4
sofatimofeeva <timofeevasofa26@gmail.com>
2025-03-04 20:42:07 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -2,24 +2,38 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def title():
     return "Missiya"
-@app.route('/image_mars')
-def return_sample_page():
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
     return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" 
-           alt="здесь должна была быть картинка, но не нашлась">
-                    <p> Вот она какая, красная планета.</p>
-                  </body>
-                </html>'''
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                      </head>
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}!</h1>
+                      <h2> Эта планета близка к Земле;</h2>
+                        <div class="alert alert-success" role="alert">
+                            <p>На ней много необходимых ресурсов;</p>
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            <p>На ней есть вода и атмосфера;</p>
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            <p>На ней есть небольшое магнитное поле;</p>
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            <p>Наконец, она просто красива!</p>
+                        </div>
+                      </body>
+                    </html>'''
 
 
 if __name__ == '__main__':
