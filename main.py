6c4228a0387ab0e5bcc0adfbe663c987f9302bdb
sofatimofeeva <timofeevasofa26@gmail.com>
2025-03-10 20:52:32 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -7,27 +7,35 @@ app = Flask(__name__)
 def title():
     return "Missiya"
 
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def choice(nickname, level, rating):
+@app.route('/promotion_image')
+def choice():
     return f'''<!doctype html>
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                       </head>
-                        <title>Результаты</title>
+                        <title>Колонизация</title>
                       </head>
                       <body>
-                        <h1>Результаты отбора</h1>
-                      <h2>Претендента на участие в миссии {nickname}:</h2>
-                        <div class="alert alert-success" role="alert">
-                            <h4 class="alert-heading">Поздравляем! Ваш рейтинг после {level} этапа отбора</h4>
+                        <h1>Жди нас Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                         <div class="alert alert-secondary" role="alert">
+                            <h4 class="alert-heading">Человечество вырастает из детства.</h4>
                         </div>
-                        <div>
-                            <strong>составляет {rating}!</strong>
+                         <div class="alert alert-success" role="alert">
+                            <h4 class="alert-heading">Человечеству мала одна планета.</h4>
                         </div>
-                        <div class="alert alert-warning f" role="alert">
-                            <h4 class="alert-heading">Желаем удачи!</h4>
+                        <div class="alert alert-secondary" role="alert">
+                            <h4 class="alert-heading">Мы сделаем обитаемыми безжизненные пока планеты<./h4>
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            <h4 class="alert-heading">И начнём с Марса!</h4>
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            <h4 class="alert-heading">Присоединяйся!</h4>
                         </div>
                       </body>
                     </html>'''
