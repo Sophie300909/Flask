237dd3bb8793bd6d4bf42e5be9917a195632a497
sofatimofeeva <timofeevasofa26@gmail.com>
2025-04-05 14:16:31 +0300

    По каютам!

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ app = Flask(__name__)
 def index(word):
     return render_template('base.html', title=word)
 
-@app.route('/answer')
-@app.route('/auto_answer')
-def answer():
-    with open("answers.json", "rt", encoding="utf8") as f:
+@app.route('/distribution')
+def distribution():
+    with open("astronauts.json", "rt", encoding="utf8") as f:
         answers_list = json.loads(f.read())
     print(answers_list)
-    return render_template('auto_answer.html', answers=answers_list)
+    return render_template('astronauts.html', astronauts=answers_list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
