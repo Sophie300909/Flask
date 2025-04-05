b24cf37dad7d491dc6e7d96a56222fc0b2d931e9
sofatimofeeva <timofeevasofa26@gmail.com>
2025-04-05 16:25:48 +0300

    Двойная защита

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-import json
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-from flask import Flask, render_template
+
+class LoginForm(FlaskForm):
+    id = StringField('Id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('Id капитана', validators=[DataRequired()])
+    captain_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 @app.route('/<word>')
 @app.route('/index/<word>')
 def index(word):
     return render_template('base.html', title=word)
 
-@app.route('/distribution')
-def distribution():
-    with open("astronauts.json", "rt", encoding="utf8") as f:
-        answers_list = json.loads(f.read())
-    print(answers_list)
-    return render_template('astronauts.html', astronauts=answers_list)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
