e6353040b866ea2c6fe8d07530d02f96247199bc
sofatimofeeva <timofeevasofa26@gmail.com>
2025-04-05 14:02:10 +0300

    Автоматический ответ

--- main.py
+++ main.py
@@ -1,97 +1,21 @@
-from flask import Flask, render_template, redirect
-from data import db_session
-from data.users import User
-from data.jobs import Jobs
-from forms.jobs import JobsForm
-from forms.user import RegisterForm, LoginForm
-from flask_login import LoginManager, login_user, logout_user, login_required
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-login_manager = LoginManager()
-login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
+@app.route('/<word>')
+@app.route('/index/<word>')
+def index(word):
+    return render_template('base.html', title=word)
 
-@app.route('/')
-def index():
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
-    print(jobs)
-    return render_template("index.html", jobs=jobs)
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            surname=form.surname.data,
-            email=form.email.data,
-            age=form.age.data,
-            position=form.position.data,
-            speciality=form.speciality.data,
-            address=form.address.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.alidate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-@app.route('/addajob', methods=['GET', 'POST'])
-def addajob():
-    form = JobsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        job = Jobs()
-        job.job = form.job.data
-        job.teamleader = form.leader.data
-        job.work_size = form.work_size.data
-        job.collaborators = form.collaborators.data
-        job.is_finished = form.is_finish.data
-        db_sess.add(job)
-        db_sess.commit()
-        return redirect("/")
-    return render_template('addajob.html',
-                               title="Добавление работы",
-                               form=form)
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-def main():
-    db_session.global_init("db/mars_explorer.db")
-    app.run()
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    with open("answers.json", "rt", encoding="utf8") as f:
+        answers_list = json.loads(f.read())
+    print(answers_list)
+    return render_template('auto_answer.html', answers=answers_list)
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
