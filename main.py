706770e7f4b4fe8512b87c44fa607dcb9ff497ef
sophie <happyskating@yandex.ru>
2025-05-15 19:50:12 +0300

    Цвет каюты

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-import os
-import sys
+from flask import Flask, render_template, request
 
-import pygame
-import requests
+app = Flask(__name__)
 
-server_address = 'https://static-maps.yandex.ru/v1?'
-api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll_spn = "ll=30.097431,59.908131&spn=0.2,0.2"
-# Готовим запрос.
 
-coords = "29.914783,59.891573,30.105881,59.944074,30.237944,59.916487,30.266268,59.919073,30.275489,59.930952,30.310165,59.941203"
-map_request = f"{server_address}{ll_spn}&apikey={api_key}&pl={coords}"
-print(map_request)
-response = requests.get(map_request)
+@app.route('/')
+def index():
+    return render_template('base.html')
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+@app.route('/table/<person>/<age>')
+def table(person, age):
+    age = int(age)
+    if age < 21:
+        photo = '/static/img/small.jpg'
+    else:
+        photo = '/static/img/big.png'
+    if person == 'male':
+        if age < 21:
+            color = 'DeepSkyBlue'
+        else:
+            color = 'LightSkyBlue'
+    else:
+        if age < 21:
+            color = 'MistyRose'
+        else:
+            color = 'Pink'
+    return render_template('astronauts.html', photo=photo, color=color)
 
-# Инициализируем pygame
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
