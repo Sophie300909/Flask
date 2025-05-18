477cb8426c458dd63d511ce8fe5d0e100d73dae9
sophie <happyskating@yandex.ru>
2025-05-18 17:37:14 +0300

    Готовое приложение

--- main.py
+++ main.py
@@ -1,32 +1,140 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from forms.jobs import JobsForm
+from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
 
 
-@app.route('/table/<person>/<age>')
-def table(person, age):
-    age = int(age)
-    if age < 21:
-        photo = '/static/img/small.jpg'
-    else:
-        photo = '/static/img/big.png'
-    if person == 'male':
-        if age < 21:
-            color = 'DeepSkyBlue'
-        else:
-            color = 'LightSkyBlue'
-    else:
-        if age < 21:
-            color = 'MistyRose'
-        else:
-            color = 'Pink'
-    return render_template('astronauts.html', photo=photo, color=color)
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form, title='Авторизация')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/addajob', methods=['GET', 'POST'])
+def addajob():
+    form = JobsForm()
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            job = Jobs()
+            job.job = form.job.data
+            job.teamleader = form.leader.data
+            job.work_size = form.work_size.data
+            job.collaborators = form.collaborators.data
+            job.is_finished = form.is_finish.data
+            db_sess.add(job)
+            db_sess.commit()
+            return redirect("/")
+        return render_template('addajob.html',
+                               title="Добавление работы",
+                               form=form)
+    return redirect('/login')
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).filter(Jobs.id == id).first()
+    form = JobsForm(
+        job=job.job,
+        leader=job.teamleader,
+        work_size=job.work_size,
+        collaborators=job.collaborators,
+        is_finish=bool(job.is_finished)
+    )
+    if form.validate_on_submit():
+        job.job = form.job.data
+        job.teamleader = form.leader.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.is_finished = form.is_finish.data
+        db_sess.merge(job)
+        db_sess.commit()
+        return redirect("/")
+    return render_template('addajob.html',
+                           title="Редактирование работы",
+                           form=form)
+
+
+@app.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).filter(Jobs.id == id).first()
+    db_sess.delete(job)
+    db_sess.commit()
+    return redirect('/')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
